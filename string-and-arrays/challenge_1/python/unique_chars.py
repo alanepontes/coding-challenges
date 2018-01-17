f963447a98239d231c3357f761d7d7485dad753b
alane <alanepontes@gmail.com>
2018-01-17 21:12:37 -0200

    implementacao com bitwise opetator AND e complexidade espacial constante

--- string-and-arrays/challenge_1/python/unique_chars.py
+++ string-and-arrays/challenge_1/python/unique_chars.py
@@ -1,13 +1,11 @@
-# Determine if a string contains only unique characters
+# challenge_1: if a string contains only unique characters
 
 class UniqueChars():
   # Implementacao com set()
   # Complexidade temporal O(n)
   # Complexidade espacial O(n)
   def withSet(self, string):
-    if string is None:
-      return False
-    return len(set(string)) == len(string)
+    return not (string is None) and len(set(string)) == len(string)
 
   def _createHashMap(self, string):
     setChars = set()
@@ -16,12 +14,26 @@ class UniqueChars():
         setChars.add(char)
     return setChars
     
-  # Implementacao com hashMap()
+  # Implementacao com hashMap lookup
   # Complexidade temporal O(n)
   # Complexidade espacial O(n)
   def withHashMapLookup(self, string):
-    if string is None:
+    return not (string is None) and len(self._createHashMap(string)) == len(string) 
+
+  # Implementacao com bitwise operator AND
+  # Complexidade temporal O(n)
+  # Complexidade espacial O(1)
+  def withBitAnd(self, string):
+    if (string is None):
       return False
-    return len(self._createHashMap(string)) == len(string) 
+    
+    accumulator = int(0)
+    for char in string:
+      val = int(2**(ord(char) - ord('A'))) 
+      if accumulator & val == 0:
+        accumulator += val
+      else:
+        return False
+    return True 
     
     
--- string-and-arrays/challenge_1/python/unique_chars.spec.py
+++ string-and-arrays/challenge_1/python/unique_chars.spec.py
@@ -10,11 +10,20 @@ class TestUniqueChars(unittest.TestCase):
     self.assertEqual(self.uniqueChars.withSet(''), True)
     self.assertEqual(self.uniqueChars.withSet('alane'), False)
     self.assertEqual(self.uniqueChars.withSet('alne'), True)
+    self.assertEqual(self.uniqueChars.withSet('alneCC'), False)
 
     self.assertEqual(self.uniqueChars.withHashMapLookup(None), False)
     self.assertEqual(self.uniqueChars.withHashMapLookup(''), True)
     self.assertEqual(self.uniqueChars.withHashMapLookup('alane'), False)
     self.assertEqual(self.uniqueChars.withHashMapLookup('alne'), True)
+    self.assertEqual(self.uniqueChars.withHashMapLookup('alneCC'), False)
+
+    self.assertEqual(self.uniqueChars.withBitAnd(None), False)
+    self.assertEqual(self.uniqueChars.withBitAnd(''), True)
+    self.assertEqual(self.uniqueChars.withBitAnd('alane'), False)
+    self.assertEqual(self.uniqueChars.withBitAnd('alne'), True)
+    self.assertEqual(self.uniqueChars.withBitAnd('alneçç'), False)
+    self.assertEqual(self.uniqueChars.withBitAnd('ààlne'), False)
        
 
 if __name__ == '__main__':
