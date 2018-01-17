7454e3d3469475aa50cb4e45c0254172a8775433
alane <alanepontes@gmail.com>
2018-01-17 18:52:46 -0200

    implementacao de solucao de challenge_1 com hashmap lookup

--- string-and-arrays/challenge_1/python/unique_chars.py
+++ string-and-arrays/challenge_1/python/unique_chars.py
@@ -1,10 +1,27 @@
 # Determine if a string contains only unique characters
 
 class UniqueChars():
-  # Implementação com set()
+  # Implementacao com set()
   # Complexidade temporal O(n)
   # Complexidade espacial O(n)
-  def withSet(string):
+  def withSet(self, string):
     if string is None:
       return False
     return len(set(string)) == len(string)
+
+  def _createHashMap(self, string):
+    setChars = set()
+    for char in string:
+      if char not in setChars:
+        setChars.add(char)
+    return setChars
+    
+  # Implementacao com hashMap()
+  # Complexidade temporal O(n)
+  # Complexidade espacial O(n)
+  def withHashMapLookup(self, string):
+    if string is None:
+      return False
+    return len(self._createHashMap(string)) == len(string) 
+    
+    
--- string-and-arrays/challenge_1/python/unique_chars.spec.py
+++ string-and-arrays/challenge_1/python/unique_chars.spec.py
@@ -3,13 +3,18 @@ from unique_chars import UniqueChars
 
 class TestUniqueChars(unittest.TestCase):
   def setUp(self):
-    self.func = UniqueChars.withSet
+    self.uniqueChars = UniqueChars()
 
   def test_all_unique_chars(self):
-    self.assertEqual(self.func(None), False)
-    self.assertEqual(self.func(''), True)
-    self.assertEqual(self.func('alane'), False)
-    self.assertEqual(self.func('alne'), True)
+    self.assertEqual(self.uniqueChars.withSet(None), False)
+    self.assertEqual(self.uniqueChars.withSet(''), True)
+    self.assertEqual(self.uniqueChars.withSet('alane'), False)
+    self.assertEqual(self.uniqueChars.withSet('alne'), True)
+
+    self.assertEqual(self.uniqueChars.withHashMapLookup(None), False)
+    self.assertEqual(self.uniqueChars.withHashMapLookup(''), True)
+    self.assertEqual(self.uniqueChars.withHashMapLookup('alane'), False)
+    self.assertEqual(self.uniqueChars.withHashMapLookup('alne'), True)
        
 
 if __name__ == '__main__':
